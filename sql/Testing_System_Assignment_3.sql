bb2fcbeb08af721b059583890cfb03badec3ba02
huytusneaker <dhtu12345@gmail.com>
2021-07-02 00:25:42 +0700

    Update Assignment 3
    nộp lại file đủ 15 question

--- sql/Testing_System_Assignment_3.sql
+++ sql/Testing_System_Assignment_3.sql
@@ -297,15 +297,59 @@ SELECT QuestionID, Count(1)  as 'socautl'
 FROM Answer
 GROUP BY QuestionID
 HAVING count(1)>=4;
+
+
+
 SELECT * FROM Answer;
 -- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày 20/12/2019
 
+SELECT ExamID
+FROM Exam
+WHERE Duration >=60;
 
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT *
+FROM `Group`
+ORDER BY CreateDate ASC
+LIMIT 5;
 
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT Count(1) AS dp2_numberOfAccount
+FROM `Account`
+WHERE DepartmentID = 2
+Group by DepartmentID;
 
+-- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
 
+SELECT * 
+FROM `account`
+WHERE fullname like 'D%o';
 
+-- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+DELETE 
+FROM	Exam
+WHERE CreateDate < '2019/12/20';
 
+-- Question 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
+Delete 
+FROM Question
+WHERE Content like 'câu hỏi %';
+
+-- Question 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và 
+--  email thành loc.nguyenba@vti.com.vn
+
+UPDATE `Account`
+SET		Fullname = 'dao ba loc'
+WHERE	AccountID= 5;
+
+UPDATE `Account`
+SET		email = 'loc.nguyenba@vti.com.vn'
+WHERE	AccountID= 5;
+
+-- Question 15: update account có id = 5 sẽ thuộc group có id = 4
+UPDATE `GroupAccount`
+SET		`GroupID` =4
+WHERE	`AccountID`= 5;
 
 
 
